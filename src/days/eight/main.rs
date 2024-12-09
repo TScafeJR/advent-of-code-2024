f06875fe2fbf7e96cd887140f86323b4e54dec67
Tyrone Scafe <tyronescafe@Tyrones-MacBook-Pro.local>
2024-12-08 22:02:58 -0500

    fix formatting

--- src/days/eight/main.rs
+++ src/days/eight/main.rs
@@ -4,7 +4,22 @@ use std::collections::{HashMap, HashSet};
 
 type Point = (usize, usize);
 
-fn calculate_antinode(dim: Point, p1: Point, p2: Point) -> Vec<Point> {
+fn calculate_antinode(
+    dim: Point,
+    p1: Point,
+    p2: Point,
+    recursive: bool,
+    v: &mut HashSet<Point>,
+) -> Vec<Point> {
+    if v.contains(&p1) && v.contains(&p2) {
+        return vec![];
+    }
+
+    if recursive {
+        v.insert(p1);
+        v.insert(p2);
+    }
+
     let horiz_offset = p2.0 as isize - p1.0 as isize;
     let vert_offset = p2.1 as isize - p1.1 as isize;
 
@@ -18,20 +33,28 @@ fn calculate_antinode(dim: Point, p1: Point, p2: Point) -> Vec<Point> {
         && antinode_1.0 < dim.0 as isize
         && antinode_1.1 < dim.1 as isize
     {
-        antinodes.push((antinode_1.0 as usize, antinode_1.1 as usize));
+        let antinode_pt = (antinode_1.0 as usize, antinode_1.1 as usize);
+        antinodes.push(antinode_pt);
+        if recursive {
+            antinodes.extend(calculate_antinode(dim, antinode_pt, p1, recursive, v));
+        }
     }
     if antinode_2.0 >= 0
         && antinode_2.1 >= 0
         && antinode_2.0 < dim.0 as isize
         && antinode_2.1 < dim.1 as isize
     {
-        antinodes.push((antinode_2.0 as usize, antinode_2.1 as usize));
+        let antinode_pt = (antinode_2.0 as usize, antinode_2.1 as usize);
+        antinodes.push(antinode_pt);
+        if recursive {
+            antinodes.extend(calculate_antinode(dim, p2, antinode_pt, recursive, v));
+        }
     }
 
     antinodes
 }
 
-fn calculate_antinodes(dim: Point, m: HashMap<char, Vec<Point>>) -> HashSet<Point> {
+fn calculate_antinodes(dim: Point, m: HashMap<char, Vec<Point>>, r: bool) -> HashSet<Point> {
     let mut set = HashSet::new();
 
     for (_key, locs) in &m {
@@ -39,12 +62,17 @@ fn calculate_antinodes(dim: Point, m: HashMap<char, Vec<Point>>) -> HashSet<Poin
             continue;
         }
 
+        if r {
+            for loc in locs {
+                set.insert(*loc);
+            }
+        }
+
         for i in 0..locs.len() - 1 {
             for j in i + 1..locs.len() {
-                let ps = calculate_antinode(dim, locs[i], locs[j]);
-                for p in ps {
-                    set.insert(p);
-                }
+                let mut visited = HashSet::new();
+                let ps = calculate_antinode(dim, locs[i], locs[j], r, &mut visited);
+                set.extend(ps);
             }
         }
     }
@@ -52,7 +80,7 @@ fn calculate_antinodes(dim: Point, m: HashMap<char, Vec<Point>>) -> HashSet<Poin
     set
 }
 
-fn part1(data: Vec<String>) -> u64 {
+fn process_data(data: Vec<String>, r: bool) -> u64 {
     let converted_data: Vec<Vec<char>> =
         data.iter().map(|l| arrays::convert_str_to_vec(l)).collect();
 
@@ -69,13 +97,17 @@ fn part1(data: Vec<String>) -> u64 {
 
     let dim = (converted_data[0].len(), converted_data.len());
 
-    let res = calculate_antinodes(dim, map);
+    let res = calculate_antinodes(dim, map, r);
 
     res.len() as u64
 }
 
-fn part2(_data: Vec<String>) -> u64 {
-    0
+fn part1(data: Vec<String>) -> u64 {
+    process_data(data, false)
+}
+
+fn part2(data: Vec<String>) -> u64 {
+    process_data(data, true)
 }
 
 pub fn functions() -> Day {
--- src/days/eight/test.rs
+++ src/days/eight/test.rs
@@ -14,7 +14,6 @@ mod tests {
         util::files::read_file_line_by_line(data_path)
     }
 
-    #[ignore]
     #[test]
     fn p1() {
         if let Some(day) = days::get_day_from_str("eight") {
@@ -190,14 +189,148 @@ mod tests {
         }
     }
 
-    #[ignore]
     #[test]
     fn p2() {
         if let Some(day) = days::get_day_from_str("eight") {
             if let Some(parsed_fns) = days::get_day(day) {
                 if let Some(part2_fn) = parsed_fns.part2 {
                     let result = part2_fn(get_data());
-                    assert_eq!(result, 31);
+                    assert_eq!(result, 34);
+                    return;
+                }
+
+                panic!("Part 2 is not supported.");
+            } else {
+                panic!("Part 2 is not supported.");
+            }
+        } else {
+            panic!("Day {} is not supported.", "eight");
+        }
+    }
+    #[test]
+    fn p2_edge_case_chain() {
+        let data = vec![
+            "....".to_string(),
+            "..A.".to_string(),
+            "...A".to_string(),
+            "....".to_string(),
+        ];
+
+        if let Some(day) = days::get_day_from_str("eight") {
+            if let Some(parsed_fns) = days::get_day(day) {
+                if let Some(part2_fn) = parsed_fns.part2 {
+                    let result = part2_fn(data);
+                    assert_eq!(result, 3);
+                    return;
+                }
+
+                panic!("Part 2 is not supported.");
+            } else {
+                panic!("Part 2 is not supported.");
+            }
+        } else {
+            panic!("Day {} is not supported.", "eight");
+        }
+    }
+
+    #[test]
+    fn p2_edge_case_overlap() {
+        let data = vec![
+            "....".to_string(),
+            "..A.".to_string(),
+            "...A".to_string(),
+            "..A.".to_string(),
+        ];
+
+        if let Some(day) = days::get_day_from_str("eight") {
+            if let Some(parsed_fns) = days::get_day(day) {
+                if let Some(part2_fn) = parsed_fns.part2 {
+                    let result = part2_fn(data);
+                    assert_eq!(result, 4);
+                    return;
+                }
+
+                panic!("Part 2 is not supported.");
+            } else {
+                panic!("Part 2 is not supported.");
+            }
+        } else {
+            panic!("Day {} is not supported.", "eight");
+        }
+    }
+
+    #[test]
+    fn p2_edge_case_boundary() {
+        let data = vec![
+            "A...".to_string(),
+            "...A".to_string(),
+            "....".to_string(),
+            "....".to_string(),
+        ];
+
+        if let Some(day) = days::get_day_from_str("eight") {
+            if let Some(parsed_fns) = days::get_day(day) {
+                if let Some(part2_fn) = parsed_fns.part2 {
+                    let result = part2_fn(data);
+                    assert_eq!(result, 2);
+                    return;
+                }
+
+                panic!("Part 2 is not supported.");
+            } else {
+                panic!("Part 2 is not supported.");
+            }
+        } else {
+            panic!("Day {} is not supported.", "eight");
+        }
+    }
+
+    #[test]
+    fn p2_large_grid() {
+        let data = vec![
+            "............".to_string(),
+            "............".to_string(),
+            "....A.......".to_string(),
+            "...........A".to_string(),
+            "............".to_string(),
+            "............".to_string(),
+            "............".to_string(),
+        ];
+
+        if let Some(day) = days::get_day_from_str("eight") {
+            if let Some(parsed_fns) = days::get_day(day) {
+                if let Some(part2_fn) = parsed_fns.part2 {
+                    let result = part2_fn(data);
+                    assert_eq!(result, 2);
+                    return;
+                }
+
+                panic!("Part 2 is not supported.");
+            } else {
+                panic!("Part 2 is not supported.");
+            }
+        } else {
+            panic!("Day {} is not supported.", "eight");
+        }
+    }
+
+    #[test]
+    fn p2_complex_grid() {
+        let data = vec![
+            "....A......".to_string(),
+            "...........".to_string(),
+            ".......A...".to_string(),
+            "...........".to_string(),
+            "....A......".to_string(),
+            "...........".to_string(),
+            "...A.......".to_string(),
+        ];
+
+        if let Some(day) = days::get_day_from_str("eight") {
+            if let Some(parsed_fns) = days::get_day(day) {
+                if let Some(part2_fn) = parsed_fns.part2 {
+                    let result = part2_fn(data);
+                    assert_eq!(result, 9);
                     return;
                 }
 
